ef1ddc5b39bfd60f7b4b8ce219f0ddb1de1406aa
liviomaya <liviomaya@gmail.com>
2022-07-19 22:53:24 -0300

    upd MetropolisHastings.jl

--- src/MetropolisHastings.jl
+++ src/MetropolisHastings.jl
@@ -1,9 +1,12 @@
 module MetropolisHastings
 
-export MHOptions, MHResults, mhsampler, confidenceinterval
+export MHOptions, MHResults, mhsampler
+export S2MHOptions, s2mhsampler
+export AMOptions, AMResults, amsampler
+export summarytable
 
 include("f0_header.jl")
-include("f1_sampler.jl")
-include("f2_functions.jl")
+include("f1_mhsampler.jl")
+include("f2_amsampler.jl")
 
 end # module
